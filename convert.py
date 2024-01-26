8acf47703ed036505062ef6467a990f1a2f43dee
AnTi3z <anti3z@anti3z.ru>
2024-01-26 18:00:38 +0300

    Compass meta item serialization implemented

--- convert.py
+++ convert.py
@@ -276,8 +276,14 @@ def serialize_meta_entity_tag(meta_item_tag):
 
 def serialize_meta_compass(meta_item_tag):
     meta = serialize_meta_item(meta_item_tag, 'COMPASS')
-    # TODO: Serialize compass (to be implemented)
-    # https://hub.spigotmc.org/stash/projects/SPIGOT/repos/craftbukkit/browse/src/main/java/org/bukkit/craftbukkit/inventory/CraftMetaCompass.java
+    if 'LodestoneDimension' in meta_item_tag and 'LodestonePos' in meta_item_tag:
+        # TODO: Check on how multiverse worlds should be saved here
+        meta['LodestonePosWorld'] = meta_item_tag['LodestoneDimension'].value
+        meta['LodestonePosX'] = meta_item_tag['LodestonePos']['LodestonePosX'].value
+        meta['LodestonePosY'] = meta_item_tag['LodestonePos']['LodestonePosY'].value
+        meta['LodestonePosZ'] = meta_item_tag['LodestonePos']['LodestonePosZ'].value
+    if 'LodestoneTracked' in meta_item_tag:
+        meta['LodestoneTracked'] = meta_item_tag['LodestoneTracked'].value
     return meta
     
 
