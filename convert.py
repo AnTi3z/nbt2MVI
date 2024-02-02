35c289646d467b845a9832158fdba1bfa0e7ce9b
AnTi3z <anti3z@anti3z.ru>
2024-02-02 09:24:15 +0300

    Bundle and HideFlags tested (works as expected)

--- convert.py
+++ convert.py
@@ -373,7 +373,6 @@ def serialize_meta_compass(meta_item_tag):
 
 
 def serialize_meta_bundle(meta_item_tag):
-    # TODO: Test Bundle
     meta = serialize_meta_item(meta_item_tag, 'BUNDLE')
     if 'Items' in meta_item_tag and len(meta_item_tag['Items']) > 0:
         meta['items'] = [serialize_item_stack(item_stack) for item_stack in meta_item_tag['Items']]
@@ -424,7 +423,6 @@ def serialize_meta_item(meta_item_tag, meta_type='UNSPECIFIC', internal_tag=None
         meta['repair-cost'] = meta_item_tag['RepairCost'].value
 
     if 'HideFlags' in meta_item_tag:
-        # TODO: Test item hide flags
         hide_flag = meta_item_tag['HideFlags'].value
         item_flags = zip(format(hide_flag, '08b'),
                          ('HIDE_ARMOR_TRIM', 'HIDE_DYE', 'HIDE_POTION_EFFECTS', 'HIDE_PLACED_ON',
